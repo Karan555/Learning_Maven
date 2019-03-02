13a1564bf218ba635a6000f13e80f89c8035e9eb
Karan555 <48146475+Karan555@users.noreply.github.com>
2019-03-02 12:56:05 +0530

    Initial Commit

---
+++ src/main/java/com/bjs/learnig/Learning_Maven/App.java
@@ -0,0 +1,13 @@
+package com.bjs.learnig.Learning_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/bjs/learnig/Learning_Maven/learn.java
@@ -0,0 +1,8 @@
+package com.bjs.learnig.Learning_Maven;
+
+public class learn {
+		public static void main(String[] args) {
+			System.out.println("maven");
+		}
+
+}
