bcea50563d7a0d091b04b114aa0017ff714f71e7
TechFundas <tech.naveenks@gmail.com>
2019-03-03 11:28:19 +0530

    added payment test case

--- src/test/java/com/bjs/learnig/Learning_Maven/AppTest.java
+++ src/test/java/com/bjs/learnig/Learning_Maven/AppTest.java
@@ -7,46 +7,52 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    public void verifylogin(){
-    	assertTrue(false);
-    }
-    public void testsignup(){
-    	assertTrue(false);
-    }
-    public void testSignIn(){
-    	assertTrue(false);
-    }
-    public void test(){
-    	assertTrue(false);
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 
-    public void testapp(){
-	assertTrue(false);}}
+	public void verifylogin() {
+		assertTrue(false);
+	}
+
+	public void testsignup() {
+		assertTrue(false);
+	}
+
+	public void testSignIn() {
+		assertTrue(false);
+	}
+
+	public void test() {
+		assertTrue(false);
+	}
+
+	public void testapp() {
+		assertTrue(false);
+	}
+
+	public void testPayment() {
+		assertTrue(false);
+	}
+}
