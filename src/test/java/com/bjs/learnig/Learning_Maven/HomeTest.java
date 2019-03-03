7979f3366079d6d2c9032f817cb5ad251252c271
Karan555 <48146475+Karan555@users.noreply.github.com>
2019-03-03 11:39:15 +0530

    added test file for home

---
+++ src/test/java/com/bjs/learnig/Learning_Maven/HomeTest.java
@@ -0,0 +1,15 @@
+package com.bjs.learnig.Learning_Maven;
+
+public class HomeTest {
+
+	public static void paymentapp(){
+		//assertTrue(false);
+		System.out.println("home");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
